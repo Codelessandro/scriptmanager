b8ca26dfef30a64046bcc09b2ccb08256bda618a
Alessandro Schneider <alessandro.schneider.privat@gmail.com>
2018-11-21 20:59:23 +0100

    pre publish

--- manager.js
+++ manager.js
@@ -1,27 +1,21 @@
-let scripts=['/scripts/scriptA.sh','/scripts/scriptB.sh']
-const fs = require('fs')
+const fs = require('fs');
 
-
-let isDone = (script) => { 
-	data = JSON.parse(fs.readFileSync('done.json', 'utf8'))
-	return data["Files"].filter( e => e["name"]==script).length>0
+const isDone = (script) => { 
+	const data = JSON.parse(fs.readFileSync('done.json', 'utf8'));
+	return data["Files"].filter( e => e["name"]==script).length>0;
 }
-
 
 function run(script) {
-	done = JSON.parse(fs.readFileSync('done.json', 'utf8'))
-	//run script here
+	const done = JSON.parse(fs.readFileSync('done.json', 'utf8'));
+	const shell = require('shelljs');
+	console.log(script)
+	shell.exec(script)
 	done["Files"].push({"name" : script})
-	console.log(typeof(done))
-	fs.writeFileSync('done.json', done);
-	//fs.writeFileSync('data.json', data)
+	fs.writeFileSync('done.json', JSON.stringify(done));
 }
 
-
-function runScripts() {
+module.exports.runScripts = function(scripts) {
 	scripts.filter( script => !isDone(script)).forEach( script => run(script)  )  
-	//run scripts
-		//if done > save to .json
 }
 
 runScripts()
--- manager.test.js
+++
@@ -1,4 +0,0 @@
-const manager = require('./manager.js')
-test('checks if detects script correctly', () => {
-	expect(manager('/scripts/scriptA.sh')).toBe(true)
-});
